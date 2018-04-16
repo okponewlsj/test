e6b2b8815c919c3d64357d184e67967865844ccb
okponewlsj <okponewlsj@naver.com>
2018-04-16 18:38:31 +0900

    그누보드 업로드

--- test2/menu/menu.php
+++ test2/menu/menu.php
@@ -1,4 +1,4 @@
-<?php include "../head.php";?>
+
 
 <div class="top_tap">
 	<a style="border-bottom: none; font-weight: bold; background-color: #fff;">굽는 메뉴</a>
@@ -8,11 +8,9 @@
 <div style="clear: both;"></div>
 
 <div class="menu_wrap">
-<a href="./menu_1.php">
 	<img alt="" src="../img/menu/menu_1_1.jpg"><br/>
 	<div>생삼겹살</div>
 	<div>100g 당 8,000원</div>	
-</a>
 <a>
 	<img alt="" src="../img/menu/menu_2_1.jpg"><br/>
 	<div>생삼겹살</div>
@@ -38,6 +36,3 @@
 </a>
 
 </div>
-
-
-<?php include "../tail.php";?>
